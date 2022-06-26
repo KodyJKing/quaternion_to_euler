30d086523b2bc80de46f607d7f643ed9e924497d
KJK <kody.j.king@gmail.com>
2022-06-25 18:55:19 -0700

    cleanup unit test

--- test.py
+++ test.py
@@ -11,25 +11,6 @@ r = Random()
 r.seed("This is the quaternion to euler angles RNG-seed.")
 np.set_printoptions(precision=3)
 
-class TestToEulerFunctions(unittest.TestCase):
-
-    def test_matrix_to_euler(self):
-        # print("\nMatrices: ")
-        for i in range(test_iterations):
-            angles_in = random_euler_angles()
-            m = euler_matrix(*angles_in)
-            angles_out = matrix_to_euler(m)
-            compare_angles(self, angles_in, angles_out)
-
-    def test_quat_to_euler(self):
-        # print("\nQuaternions: ")
-        for i in range(test_iterations):
-            angles_in = random_euler_angles()
-            q = euler_quat(*angles_in)
-            angles_out = quat_to_euler(q)
-            compare_angles(self, angles_in, angles_out)
-
-
 test_iterations = 1000
 degree_tolerance = 1e-6
 
@@ -47,6 +28,22 @@ def compare_angles(test: unittest.TestCase, angles_in, angles_out):
     # print("   Err:", angle_errors)
     for e in angle_errors:
         test.assertTrue(e < degree_tolerance)
+
+class TestToEulerFunctions(unittest.TestCase):
+
+    def test_matrix_to_euler(self):
+        for i in range(test_iterations):
+            angles_in = random_euler_angles()
+            m = euler_matrix(*angles_in)
+            angles_out = matrix_to_euler(m)
+            compare_angles(self, angles_in, angles_out)
+
+    def test_quat_to_euler(self):
+        for i in range(test_iterations):
+            angles_in = random_euler_angles()
+            q = euler_quat(*angles_in)
+            angles_out = quat_to_euler(q)
+            compare_angles(self, angles_in, angles_out)
 
 if __name__ == "__main__":
     unittest.main()
